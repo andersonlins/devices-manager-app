3c68bdd86f2bd95bc7af57b426c8f92baeef02dc
Anderson Lins <anderson.lins101@gmail.com>
2021-05-09 15:30:51 -0400

    Creating basic api paths on environments.ts

--- client/device-manager/src/environments/environment.ts
+++ client/device-manager/src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api : {
+    baseUrl: 'http://localhost:3000',
+    paths: {
+      devices: '/devices',
+      categories: '/categories'
+    }
+  }
 };
 
 /*
