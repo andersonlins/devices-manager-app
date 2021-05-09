90f829bb209397282bbd5d1e45d986c9959d710b
Anderson Lins <anderson.lins101@gmail.com>
2021-05-09 03:57:43 -0400

    Add client draft of the project;   - Add angulr project, Home component, Category component using Angular Material

---
+++ client/device-manager/src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [
+    MatSidenavModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ client/device-manager/src/app/category/category.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListCategoryComponent } from './list-category/list-category.component';
+
+
+
+@NgModule({
+  declarations: [
+    ListCategoryComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CategoryModule { }
