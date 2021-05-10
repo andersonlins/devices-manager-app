0089596ffeb3803187f06f21cc6c205bc9c43e99
Anderson Lins <anderson.lins101@gmail.com>
2021-05-10 03:58:20 -0400

    Adding feature to delete categories

--- client/device-manager/src/app/components/pages/category/category.component.ts
+++ client/device-manager/src/app/components/pages/category/category.component.ts
@@ -1,5 +1,9 @@
+import { SNACK_PRESETS } from './../../../core/utils';
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { CategoryModel } from '@models/category.model';
+import { ModalConfirmComponent } from '../../modal-confirm/modal-confirm.component';
 import { CategoriesService } from './../../../core/services/categories.service';
 
 interface CategoryViewModel extends CategoryModel{
@@ -12,10 +16,11 @@ interface CategoryViewModel extends CategoryModel{
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-  displayedColumns: string[] = ['id', 'name'];
+  displayedColumns: string[] = ['id', 'name', 'actions'];
   categories: CategoryViewModel[] = [];
 
-  constructor(private catService: CategoriesService) { }
+  constructor(private catService: CategoriesService,
+    public dialog: MatDialog, private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.populate();
@@ -30,5 +35,22 @@ export class CategoryComponent implements OnInit {
     } catch (error) {
       console.log(error);
     }
+  }
+  deleteCategory(category: CategoryViewModel) {
+    const dialogRef = this.dialog.open(ModalConfirmComponent, {
+      data: {
+        title: 'Deseja remover "' + category.name + '"?'
+      }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+          this.catService.delete(category.id).subscribe(res => {
+          this.categories = this.categories.filter( d => d.id !== category.id);
+          this.snackBar.open('Removido com sucesso', undefined, SNACK_PRESETS.SUCCESS);
+        }, () => {
+          this.snackBar.open('Erro ao remover', undefined, SNACK_PRESETS.ERROR);
+        })
+      }
+    });
   }
 }
